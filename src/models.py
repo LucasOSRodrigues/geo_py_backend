6f79b8c1c0c1c0fafd2566a2f6f08f07bbca0d13
Lucas O. S. Rodrigues <lucasosrodrigues13@gmail.com>
2025-07-20 18:20:57 -0300

    feat: define os models

---
+++ src/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+import uuid
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    email = Column(String, primary_key=True, index=True)
+    nome = Column(String)
+
+    pontos = relationship("Ponto", back_populates="usuario")
+
+class Ponto(Base):
+    __tablename__ = "pontos"
+    id = Column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
+    latitude = Column(Float)
+    longitude = Column(Float)
+    descricao = Column(String)
+
+    usuario_email = Column(String, ForeignKey("usuarios.email"))
+    usuario = relationship("Usuario", back_populates="pontos")
